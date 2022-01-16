3a3903348f5de6a95a517e527052aefc934c7106
Reka <61565941+Rekaptcha@users.noreply.github.com>
2022-01-16 17:40:19 +0100

    Create documentation.py

---
+++ documentation.py
@@ -0,0 +1,23 @@
+"""DOCUMENTACIÓN DEL CÓDIGO"""
+"""Funciones"""
+#Cómo documentar nuestras funciones en python: en bloques de string literales englobados entre triple comilla doble o simple, a continuación de la definición de la función (def nombre_funcion(argumentos)):) comenzaría el comentario.
+
+#Ejemplo:
+
+"""Esta función realiza X operación
+Parameters
+----------
+argumento1 : [tipo_datos]
+    Una lista de tipo_datos no vacía
+
+Returns
+-------
+tipo_datos
+    Deveulve los datos después de haberles aplicado X función
+    
+Example
+-------
+>>>> Ejemplo de entrada y output de salida
+
+"""
+

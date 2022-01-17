67ae3423cea2c8a96197458ab8f45000868abb58
Reka <61565941+Rekaptcha@users.noreply.github.com>
2022-01-17 17:22:42 +0100

    Create lib-scipy.py

---
+++ Librerías/lib-scipy.py
@@ -0,0 +1,17 @@
+"""LIBRERÍA SCIPY en PYTHON"""
+"""La librería scipy se trata en realidad de una colección de librerías, tiene módulos de algoritmo de clúster, constantes matemáticas y físicas, interpolación, álgebra lineal... Y, de hecho, las librerías numpy, matplotlib, IPythn, Sympy y Pandas podrían considerarse módulos de la librería Scipy, aunque por su relevancia se suelan estudiar por separado."""
+
+"""MÓDULO stats, para distribuciones y funciones estadísticas"""
+from scipy import stats
+
+#Para consultar ayuda específica:
+help(funcion)
+
+#Distribución normal:
+from scipy.stats import norm #Módulo correspondiente a las funciones de distribución normal
+
+"""Módulo optimize, para optimización"""
+
+"""Módulo interpolate, para interpolación"""
+
+"""Módulo linalg, para álgebra lineal"""
